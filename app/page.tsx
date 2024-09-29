73f4f9e5890716a1abb50cb563590d08da78e6d1
Gleb Myznikov <myznikovgleb@yandex.ru>
2024-09-29 17:02:57 +0300

    Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import { Layout, Section } from '@/src/shared/ui'
 import { FeedbackForm } from '@/src/widgets/feedback-form'
+import { HeroSection } from '@/src/widgets/hero'
 
 export default function Page() {
   return (
     <Layout>
-      <Section>
-        <div className="flex flex-col items-center gap-6">
-          <h1 className="text-4xl font-semibold">Konspekt Docs</h1>
-          <span className="text-5xl">📝</span>
-        </div>
-      </Section>
+      <HeroSection />
       <Section>
         <FeedbackForm />
       </Section>
---
+++ src/widgets/hero/ui/hero-section.tsx
@@ -0,0 +1,21 @@
+import { Section } from '@/src/shared/ui'
+
+const HeroSection = () => {
+  return (
+    <Section>
+      <div className="flex h-4/5 w-full flex-col items-center justify-center gap-32 bg-gradient-to-tr from-yellow-300 to-orange-500 md:h-2/3 md:w-4/5 md:gap-16 md:rounded-xl">
+        <div className="flex w-full flex-col items-center justify-center gap-4 md:gap-8">
+          <h1 className="text-4xl font-semibold text-primary-content md:text-6xl">
+            Konspekt Docs
+          </h1>
+          <p className="text-2xl text-primary-content md:text-4xl">
+            Documentation for Konspekt App
+          </p>
+        </div>
+        <span className="text-8xl">📝</span>
+      </div>
+    </Section>
+  )
+}
+
+export { HeroSection }
