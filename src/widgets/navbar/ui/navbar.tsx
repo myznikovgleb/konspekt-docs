1852531943a054ee39c39c507920000a598d7113
Gleb Myznikov <42252206+myznikovgleb@users.noreply.github.com>
2025-03-27 21:53:40 +0300

    Patch (#4)
    * Update deps
    
    * Adjust navbar layout

--- src/widgets/navbar/ui/navbar.tsx
+++ src/widgets/navbar/ui/navbar.tsx
@@ -11,11 +11,11 @@ const Navbar = () => {
     <section className="fixed top-0 z-10 flex w-full flex-col gap-1 rounded-b-md bg-base-100 px-4 py-1 shadow shadow-base-200">
       <div className="navbar hidden min-h-8 justify-between p-0 md:flex">
         <div className="w-32" />
-        <Link href="/" className="btn btn-xs w-32">
+        <Link href="/" className="btn btn-xs w-36">
           <ArrowDownCircleIcon className="size-4" />
           <span>Konspekt Docs</span>
         </Link>
-        <Link href="https://konspekt.vercel.app/" className="btn btn-xs w-32">
+        <Link href="https://konspekt.vercel.app/" className="btn btn-xs w-36">
           <ArrowTopRightOnSquareIcon className="size-4" />
           <span>Konspekt App</span>
         </Link>
